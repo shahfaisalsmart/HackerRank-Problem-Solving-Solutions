52496f0512b3b049291c3e0b3313ee6cf411d2ea
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-09-05 16:12:58 +0530

    Including the O(N) approach.

--- theCoinChange.cpp
+++ theCoinChange.cpp
@@ -117,3 +117,44 @@ vector<string> split(const string &str) {
 
     return tokens;
 }
+// Another Method which computes the answer in O(n)
+
+// #include<bits/stdc++.h>
+// #define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+// using namespace std;
+// int main()
+//  {
+//     fast
+//     int t;
+//     cin >> t;
+//     while(t--)
+//     {
+//         int n;
+//         cin >> n;
+//         int * arr = new int[n];
+//         for(int i=0;i<n;i++)
+//             cin >> arr[i];
+//         int money;
+//         cin >> money;
+        
+//         int * numWays = new int [money+1];
+        
+//         numWays[0] = 1;
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=arr[i];j<=money;j++)
+//             {
+//                 numWays[j] += numWays[j-arr[i]];
+//             }
+//         }
+//         cout<<numWays[money]<<endl;
+//         // delete [] numWays;
+//         // delete [] arr;
+        
+//     }
+//     // delete [] numWays;
+//     // delete [] arr;
+// 	//code
+// 	return 0;
+// }
